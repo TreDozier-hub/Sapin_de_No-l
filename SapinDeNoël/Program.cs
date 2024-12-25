d5f7a7223df447f140d17639edd1398eea28c59e
TreDozier-hub <arnaud.dozier@gmail.com>
2024-12-25 23:20:58 +0100

    Update Program.cs

--- SapinDeNoël/Program.cs
+++ SapinDeNoël/Program.cs
@@ -82,7 +82,7 @@ namespace MyApp
                 floconsY[i] = random.Next(0, hauteurConsole);
             }
 
-            // générer et animer la neige
+            // gcréer et animer la neige
             void GenererNeige()
             {
                 for (int i = 0; i < floconsX.Length; i++)
@@ -107,7 +107,7 @@ namespace MyApp
                 Console.ResetColor();
             }
             
-            // Boucle principale pour l'animation
+            // Boucle  pour l'animation
             while (true)
             {
                 GenererNeige();
@@ -129,31 +129,6 @@ namespace MyApp
 
 
 
-//using System;
-//using System.ComponentModel.Design;
-
-
-//namespace MyApp
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            // saisie de la hauteur du sapin 
-//            Console.Write("Choisissez la taille de votre sapin entre 0 et 30 : ");
-//            int hauteurSapin = int.Parse(Console.ReadLine());
-
-//            // contrôle de saisie de la hauteur du sapin + ajouter le contrôle de saisie de caractères 
-//            while (hauteurSapin < 0 || hauteurSapin > 30)
-//            { Console.WriteLine("Merci de saisir une hauteur comprise entre 1 et 30"); }
-//            Console.Clear();
-
-//            int nombreFeuilles = 0;
-//            string rangFeuilles;
-//            int largeurMax = 120;
-//            int posInitiale = largeurMax / 2;
-//            Random boule = new Random();
-//            int proba = boule.Next(1, 10);
 
 
 //            int DessinerFeuilles(int hauteurSapin)
